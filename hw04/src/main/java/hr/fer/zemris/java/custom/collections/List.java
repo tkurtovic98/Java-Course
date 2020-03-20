1f4e0deb7d4f98997a91383adcf44d34917e50dd
t.kurtovic <tk50908@fer.hr>
2020-03-20 15:16:06 +0100

    Update: Add zip archives

--- hw04/src/main/java/hr/fer/zemris/java/custom/collections/List.java
+++
@@ -1,8 +0,0 @@
-package hr.fer.zemris.java.custom.collections;
-
-public interface List<T> extends Collection<T> {
-	T get(int index);
-	void insert(T value, int position);
-	int indexOf(Object value);
-	void remove(int index);
-}
